7406b9d6d63dfa58b364ec81fa619f75bdec7596
VinterNacht <crannnabeatha@gmail.com>
2021-12-23 15:12:12 -0600

    Add project files.

---
+++ InDappledGroves.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Vintagestory.API.Common;
+using VinterTweaks.Items.Tools;
+
+namespace InDappledGroves
+{
+    public class InDappledGroves : ModSystem
+    {
+        public override void Start(ICoreAPI api)
+        {
+            base.Start(api);
+            api.RegisterItemClass("itemvtaxe", typeof(ItemIDGAxe));
+            api.RegisterItemClass("itemvtsaw", typeof(ItemIDGSaw));
+        }
+    }
+}
